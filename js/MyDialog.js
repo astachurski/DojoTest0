6e2d2221c57b9b286debac1d53c8607e022cd7db
adrian <astachurski@gmail.com>
2014-11-11 20:47:09 +0100

    hdthdthhndt

---
+++ js/MyDialog.js
@@ -0,0 +1,23 @@
+/**
+ * Created by adrianSSD on 11/11/2014.
+ */
+define(["dojo/_base/declare", "dijit/Dialog"], function(declare, Dialog) {
+
+    return declare("demo.MyDialog", Dialog, {
+        authorNode: null,
+        authorAvatarSrc: "",
+        authorName: "",
+        //templateString: dojo.cache("demo", "AuthoredDialogTemplate.html"),
+        postMixInProperties: function() {
+            this.inherited(arguments);
+
+            if(!this.hasOwnProperty("authorName") && dojo.config.authorName) {
+                this.authorName = dojo.config.authorName;
+            }
+            if(!this.hasOwnProperty("authorAvatarSrc") && dojo.config.authorAvatarSrc) {
+                this.authorAvatarSrc = dojo.config.authorAvatarSrc;
+            }
+        }
+    });
+
+});
